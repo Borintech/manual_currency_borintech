c6aebe0eb5ed03379ebb01508c4c2df906e1958a
Nelson Tontarelli <nelartz01@gmail.com>
2023-02-10 16:40:55 -0300

    Tipo de cambio funcionando

--- custom_rate/models/models.py
+++ custom_rate/models/models.py
@@ -35,47 +35,57 @@ class AccountMove(models.Model):
             if rec.es_manual_rate:
                 for line in rec.line_ids:
 
-                    company_currency = line.account_id.company_id.currency_id
-                    balance = line.amount_currency
-                    debe = 0
-                    haber = 0
-                    if line.currency_id and company_currency and line.currency_id != company_currency:
-                        if line.move_id.purchase_currency_rate > 0:
+                    precio_credit = line.credit
+                    precio_debit = line.debit
 
-                            debe = balance * rec.currency_rate
+                    if line.amount_currency < 0:
+                        precio_credit = 0
+                        precio_credit = line.amount_currency * rec.currency_rate
+                        precio_credit = precio_credit * -1
 
-                            if line.tax_ids.amount != 0:
-                                debe = debe * line.tax_ids.amount / 100
 
-                            debe = line.currency_id._convert(debe, company_currency, line.account_id.company_id,
-                                                                line.move_id.date or fields.Date.today(), True,
-                                                                line.move_id.purchase_currency_rate)
-                        else:
+                    else:
+                        precio_debit = 0
+                        precio_debit = line.amount_currency * rec.currency_rate
 
-                            haber = balance * rec.currency_rate
+                    line.with_context(check_move_validity=False).write({
+                        'credit': precio_credit,
+                        'debit': precio_debit
+                    })
 
-                            if line.tax_ids.amount != 0:
-                                haber = haber * line.tax_ids.amount / 100
+            # res = super(AccountMove, self.with_context(check_move_validity=False)).write(vals)
 
-                            haber = line.currency_id._convert(debe, company_currency, line.account_id.company_id,
-                                                                line.move_id.date or fields.Date.today(), True,
-                                                                line.move_id.purchase_currency_rate)
+            # WRITE
+            # rec.line_ids.with_context(check_move_validity=False).write({
+            #     'credit': precio_venta,
+            #     'debit': precio_deudores
+            # })
 
-                        line.debit = debe > 0 and debe or 0.0
-                        line.credit = haber < 0 and -haber or 0.0
+            # UPDATE
+            # rec.line_ids.update({
+            #     'credit': precio_venta,
+            #     'debit': precio_deudores
+            # })
 
-                    # if line.amount_currency < 0:
-                    #     importe_moneda = line.amount_currency * -1
-                    #     haber = importe_moneda * rec.currency_rate
-                    #     if line.tax_ids.amount != 0:
-                    #         haber = haber * line.tax_ids.amount / 100
-                    #
-                    #     line.credit = haber
-                    #
-                    # else:
-                    #     importe_moneda = line.amount_currency
-                    #     debe = importe_moneda * rec.currency_rate
-                    #     if line.tax_ids.amount != 0:
-                    #         debe = debe * line.tax_ids.amount / 100
-                    #
-                    #     line.debit = debe
+            # if line.amount_currency < 0:
+            #     importe_moneda = line.amount_currency * -1
+            #     haber = importe_moneda * rec.currency_rate
+            #     if line.tax_ids.amount != 0:
+            #         haber = haber * line.tax_ids.amount / 100
+            #
+            # else:
+            #     debe += haber
+            #     importe_moneda = line.amount_currency
+            #     debe = importe_moneda * rec.currency_rate
+            #     if line.tax_ids.amount != 0:
+            #         debe = debe * line.tax_ids.amount / 100
+
+            # rec.line_ids.write({
+            #     'credit': haber,
+            #     'debit': debe
+            # })
+
+            # rec.line_ids.update({
+            #     'credit': haber,
+            #     'debit': debe
+            # })
