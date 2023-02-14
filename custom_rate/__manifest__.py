ff70ad8c0ecd15f51f390cb9833c44031768e967
MatiasBanega <banegamatias@gmai.com>
2023-02-14 17:15:28 -0300

    version final de manual currency

--- custom_rate/__manifest__.py
+++ custom_rate/__manifest__.py
@@ -3,14 +3,14 @@
     'name': "Custom rate",
 
     'summary': """
-        take the custom rate for calculate amount other currency """,
+        take the custom rate for calculate amount other currency in account move lines and modify l10n_ar_currency_rate with currency_rate """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "Exemax, Gabriela Riquelme",
-    'website': "http://www.Exemax.com.ar",
+    'author': "Devman",
+    'website': "http://www.devman.com.ar",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
